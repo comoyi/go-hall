df1a3109973259cc7e80fd7c5fe069e4cae2c0f8
Michael Chi <chicong@outlook.com>
2017-11-02 09:56:34 +0800

    websocket connect

---
+++ client.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"github.com/gorilla/websocket"
+	log "github.com/sirupsen/logrus"
+)
+
+type Client struct {
+	id    int
+	conn  *websocket.Conn
+	queue chan []byte
+}
+
+func (c *Client) Read() {
+	defer func() {
+		c.conn.Close()
+	}()
+	for {
+		messageType, message, err := c.conn.ReadMessage()
+		if err != nil {
+			log.Info("read error", err, messageType, message)
+			delete(clients, c)
+			break
+		}
+		log.Info("[recv data]", "[client id:", c.id, "] ", "[message type:", messageType, "] ", "[message:", string(message), "]")
+		for i := range clients {
+			i.queue <- message
+		}
+	}
+}
+
+func (c *Client) Write() {
+	defer func() {
+		c.conn.Close()
+	}()
+	for {
+		message, _ := <-c.queue
+		log.Info("[send data]", "[client id:", c.id, "] ", string(message))
+		err := c.conn.WriteMessage(websocket.TextMessage, message)
+		if err != nil {
+			log.Warning(err, c.id)
+			break
+		}
+	}
+}
---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"flag"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", 404)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", 405)
+		return
+	}
+	http.ServeFile(w, r, "index.html")
+}
+
+var clients = make(map[*Client]bool)
+var nextClientId int = 0
+
+func main() {
+	var addr = flag.String("p", ":8080", "http service address")
+
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(w, r)
+	})
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	client := new(Client)
+	nextClientId++
+	client.id = nextClientId
+	client.conn = conn
+	client.queue = make(chan []byte, 256)
+	clients[client] = true
+
+	go client.Read()
+	go client.Write()
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
